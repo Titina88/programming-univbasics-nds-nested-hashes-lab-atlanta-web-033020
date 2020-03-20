dc2fccc3234330217a818e7219bed4154336b020
Christina Placide <christina.placide@hotmail.com>
2020-03-20 18:02:15 +0000

    Automatically backed up by Learn

---
+++ pass.rb
@@ -0,0 +1,12 @@
+array =[1,2,3,4,5,6]
+
+def odd
+   oddly = []
+   index = 0 
+   while index < array.length do
+    if array[index] %2 ==1
+      oddly.push(array[index])
+   end 
+   index +=1
+end  
+end
